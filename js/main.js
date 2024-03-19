a168d9b79ec9d79f065ae8b2e89cd0ddfc82a628
Matías Villarroel <cmvillarroel@icloud.com>
2024-03-19 19:11:17 -0300

    Segunda preentrega

--- js/main.js
+++ js/main.js
@@ -1,29 +1,45 @@
-let nombreUsuario = prompt("Ingrese su Nombre")
-let apellidoUsiario = prompt("Ingrese su Apellido")
-let usuario = (nombreUsuario + " " + apellidoUsiario)
-let nivelUsuario;
-do {
-    nivelUsuario = prompt("Ingrese el número que corresponda a su nivel de Jiujitsu: \n1. Cinturón Blanco \n2. Cinturón Azul \n3. Cinturón Morado \n4. Cinturón Café \n5. Cinturón Negro")
-    if ((isNaN(nivelUsuario)) || (nivelUsuario > 5)){
-        alert("Ingrese el número de su opción (del 1 al 5).");
-    }
-} while ((isNaN(nivelUsuario)) || (nivelUsuario > 5));
+const cursos = [
+  {
+    nombre: "Principiantes",
+    nivel: "Básico",
+    horario: "Lunes a Viernes 18:00 - 19:30"
+  },
+  {
+    nombre: "Intermedios",
+    nivel: "Intermedio",
+    horario: "Lunes a Viernes 13:00 - 14:30"
+  },
+  {
+    nombre: "Avanzados",
+    nivel: "Avanzado",
+    horario: "Lunes a Viernes 20:00 - 21:30"
+  },
+];
 
-function obtenerRecomendacion (nivelParam) {
-    switch (nivelParam) {
-        case "1":
-            return "Para tu nivel, recomendamos que te inscribas en los cursos de fundamentos y los repases hasta que los domines a la perfección";
-        case "2":
-            return "Para tu nivel, te recomendamos elegir los cursos intermedios y avanzados de la o las técnicas que más te gusten para comenzar para comenzar a buscar y desarrollar tu juego propio. \nTambién te recomendamos inscribirte en los cursos de preparación física para mejorar tu resistencia muscular.";
-        case "3":
-            return "Para tu nivel, te recomendamos revisar a profundidad los cursos avanzados de las técnicas que ya dominas hasta controlar todos sus detalles, transiciones, contras y amenazas múltiples.";
-        case "4":
-            return "Para tu nivel, te recomendamos revisar los cursos de leg locks."
-        case "5":
-            return "Bienvenido Mestre, puedes eligir el curso que más prefieras según lo que quieras reforzar.";
+function organizarCursosPorNivel(cursos) {
+  const cursosPorNivel = {};
+  cursos.forEach(curso => {
+    if (!cursosPorNivel[curso.nivel]) {
+      cursosPorNivel[curso.nivel] = [];
     }
+    cursosPorNivel[curso.nivel].push(curso);
+  });
+  return cursosPorNivel;
 }
 
-let recomendacion = obtenerRecomendacion(nivelUsuario);
+const cursosPorNivel = organizarCursosPorNivel(cursos);
 
-alert(usuario + ": " + recomendacion);
+function obtenerCursosPorNivel(nivel) {
+  return cursosPorNivel[nivel] || [];
+}
+
+const nivelUsuario = prompt("¿Cuál es tu nivel de jiu-jitsu?");
+const cursosDisponibles = obtenerCursosPorNivel(nivelUsuario);
+if (cursosDisponibles.length > 0) {
+  console.log("Los cursos disponibles para el nivel " + [nivelUsuario] + " son:");
+  cursosDisponibles.forEach(curso => {
+    console.log([curso.nombre] + " " + [curso.horario]);
+  });
+} else {
+  console.log("Lo siento, no hay cursos disponibles para el nivel " + [nivelUsuario]);
+}
